4d4e27004f529f4beb21cd3ff8b16f071c4a407f
jeo19 <terrakkk@outlook.com>
2022-11-29 17:23:31 +0800

    feat:Add the AroundYou component step2

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -4,3 +4,18 @@ import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
 import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+const CountryTracks = () => {
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  
+    useEffect(() => {
+      axios
+        .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+        .then((res) => setCountry(res?.data?.location.country))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
+    }, [country]);
+    return()
+}
